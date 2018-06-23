b5b45d74d42a5a1c2f9ae246ddad3ae5327807cc
Matthew Cranford <mcranfordmail@gmail.com>
2018-06-23 15:53:29 -0700

    Add hero

--- js/app.js
+++ js/app.js
@@ -24,6 +24,61 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Hero {
+    constructor(sprite, x, y) {
+        this.sprite = sprite;
+        this.x = x;
+        this.y = y;
+    }
+    // Update
+    update() {
+
+    }
+    // Render hero
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+    // Handle user keyboard input
+    handleInput(key) {
+        const LEFT_WALL = 0;
+        const RIGHT_WALL = 404;
+        const TOP_WALL = 53;
+        const BOTTOM_WALL = 385;
+
+        switch (key) {
+            case 'left':
+                if (this.x > LEFT_WALL) {
+                    this.x -= CANVAS_TILE_WIDTH;
+                }
+                break;
+            case 'right':
+                if (this.x < RIGHT_WALL) {
+                    this.x += CANVAS_TILE_WIDTH;
+                }
+                break;
+            case 'up':
+               if (this.y > TOP_WALL) {
+                    this.y -= CANVAS_TILE_HEIGHT;
+                }
+                break;
+            case 'down':
+                if (this.y < BOTTOM_WALL) {
+                    this.y += CANVAS_TILE_HEIGHT;
+                }
+                break;
+        }
+        console.log(this.x,this.y)
+    }
+}
+
+const CANVAS_TILE_WIDTH = 101;
+const CANVAS_TILE_HEIGHT = 83;
+const startingTileX = Math.floor(CANVAS_TILE_WIDTH * 2);
+const startingTileY = Math.floor(CANVAS_TILE_HEIGHT * 5 -30);
+
+const player = new Hero('images/char-boy.png', startingTileX, startingTileY);
+
+
 
 
 // Now instantiate your objects.
@@ -39,7 +94,11 @@ document.addEventListener('keyup', function(e) {
         37: 'left',
         38: 'up',
         39: 'right',
-        40: 'down'
+        40: 'down',
+        65: 'left',
+        87: 'up',
+        68: 'right',
+        83: 'down'
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
